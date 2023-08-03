5b780cc378bca2e62d54121617d18efd483bd7f7
Mohmmad Mihdi <supermimo7@gmail.com>
2023-08-03 20:45:40 +0300

    adding NotificationsScreen & friendRequestScreen

--- lib/modules/friends_request_screen/friends_request_screen.dart
+++ lib/modules/friends_request_screen/friends_request_screen.dart
@@ -1,21 +1,230 @@
-import 'package:dev_space/layout/cubit/dev_cubit.dart';
+import 'package:dev_space/shared/components/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FriendRequestScreen extends StatelessWidget {
   const FriendRequestScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<DevCubit, DevState>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        return const Center(
-          child: Text(
-              'Friend Request Screen'
+    return Column(
+        children: [
+          MaterialButton(
+            onPressed:(){},
+            child: Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius:40,
+                    backgroundImage:AssetImage(
+                      '',
+                    ),
+
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment:CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Ibrahim Alnaser',
+                      style:TextStyle(
+                        color:Constants.color,
+                        fontSize:30,
+
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration:BoxDecoration(
+                            color:Constants.color,
+                            borderRadius:BorderRadius.circular(10),
+                          ),
+                          child: MaterialButton(onPressed: (){},
+                            child: const Text(
+                              'Confirm',
+                              style:TextStyle(
+                                color:Colors.white,
+                                fontSize:15,
+
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width:10,
+                        ),
+                        Container(
+                          decoration:BoxDecoration(
+                            color:Constants.color,
+                            borderRadius:BorderRadius.circular(10),
+                          ),
+                          child: MaterialButton(onPressed: (){},
+                            child: const Text(
+                              'Delete',
+                              style:TextStyle(
+                                color:Colors.white,
+                                fontSize:15,
+
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        );
-      },
+          const SizedBox(
+            height:20,
+          ),
+          MaterialButton(
+            onPressed:(){},
+            child: Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius:40,
+                    backgroundImage:AssetImage(
+                      '',
+                    ),
+
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment:CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Nissren Ahmed',
+                      style:TextStyle(
+                        color:Constants.color,
+                        fontSize:30,
+
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration:BoxDecoration(
+                            color:Constants.color,
+                            borderRadius:BorderRadius.circular(10),
+                          ),
+                          child: MaterialButton(onPressed: (){},
+                            child: const Text(
+                              'Confirm',
+                              style:TextStyle(
+                                color:Colors.white,
+                                fontSize:15,
+
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width:10,
+                        ),
+                        Container(
+                          decoration:BoxDecoration(
+                            color:Constants.color,
+                            borderRadius:BorderRadius.circular(10),
+                          ),
+                          child: MaterialButton(onPressed: (){},
+                            child: const Text(
+                              'Delete',
+                              style:TextStyle(
+                                color:Colors.white,
+                                fontSize:15,
+
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height:20,
+          ),
+          MaterialButton(
+            onPressed:(){},
+            child: Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius:40,
+                    backgroundImage:AssetImage(
+                      '',
+                    ),
+
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment:CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Leen Nasser',
+                      style:TextStyle(
+                        color:Constants.color,
+                        fontSize:30,
+
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration:BoxDecoration(
+                            color:Constants.color,
+                            borderRadius:BorderRadius.circular(10),
+                          ),
+                          child: MaterialButton(onPressed: (){},
+                            child: const Text(
+                              'Confirm',
+                              style:TextStyle(
+                                color:Colors.white,
+                                fontSize:15,
+
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width:10,
+                        ),
+                        Container(
+                          decoration:BoxDecoration(
+                            color:Constants.color,
+                            borderRadius:BorderRadius.circular(10),
+                          ),
+                          child: MaterialButton(onPressed: (){},
+                            child: const Text(
+                              'Delete',
+                              style:TextStyle(
+                                color:Colors.white,
+                                fontSize:15,
+
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height:20,
+          ),
+        ]
     );
   }
 }
+
--- lib/modules/notifications_screen/notifications_screen.dart
+++ lib/modules/notifications_screen/notifications_screen.dart
@@ -1,21 +1,138 @@
-import 'package:dev_space/layout/cubit/dev_cubit.dart';
+import 'package:dev_space/shared/components/constants.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<DevCubit, DevState>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        return const Center(
-          child: Text(
-              'Notifications Screen'
+    return Column(
+      children: [
+        MaterialButton(
+          onPressed:(){},
+          child: const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius:40,
+                  backgroundImage:AssetImage(
+                    '',
+                  ),
+
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'You Were Mentioned By Mohamed Khair In A Comment In Tecno .'
+                  ,
+                  style:TextStyle(
+                    color:Constants.color,
+                    fontSize:20,
+
+                  ),
+                ),
+              ),
+            ],
           ),
-        );
-      },
+        ),
+        const SizedBox(
+          height:20,
+        ),
+        MaterialButton(
+          onPressed:(){},
+          child: const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius:40,
+                  backgroundImage:AssetImage(
+                    '',
+                  ),
+
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'Farah Alazzam And 9 Others Recently Interacted With Your Post On Smile.'
+                  ,
+                  style:TextStyle(
+                    color:Constants.color,
+                    fontSize:20,
+
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height:20,
+        ),
+        MaterialButton(
+          onPressed:(){},
+          child: const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius:40,
+                  backgroundImage:AssetImage(
+                    '',
+                  ),
+
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'A Post Of Yours In The Laptop Group Got Liked By Yasser And 2 Other .'
+                  ,
+                  style:TextStyle(
+                    color:Constants.color,
+                    fontSize:20,
+
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height:20,
+        ),
+        MaterialButton(
+          onPressed:(){},
+          child: const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius:40,
+                  backgroundImage:AssetImage(
+                    '',
+                  ),
+
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'People Talk About Your Shopping post .'
+                  ,
+                  style:TextStyle(
+                    color:Constants.color,
+                    fontSize:20,
+
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+      ],
     );
+
   }
 }
